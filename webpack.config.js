bccf5f602623672e6e7963519798943977f6d0f2
Enodi <enodiaudu5@gmail.com>
2018-11-20 01:38:16 +0100

    setup production build and server

--- webpack.config.js
+++ webpack.config.js
@@ -45,8 +45,7 @@ module.exports = (env) => {
     devtool: isProduction ? "source-map" : "inline-source-map",
     devServer: {
       contentBase: path.join(__dirname, "public"),
-      historyApiFallback: true,
-      port: 9000
+      historyApiFallback: true
     }
   }
 };
